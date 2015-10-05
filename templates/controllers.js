d9afe4c0aafb1651b42f48a7f4283cf0548c1fa9
mvmpvinicius <mvmpvinicius@gmail.com>
2015-10-05 17:53:04 -0300

    Correções - Último

--- templates/controllers.js
+++ templates/controllers.js
@@ -15,25 +15,25 @@ app.controller('diarioController', function($scope) {
     var $diario = this;
     $diario.titulo = "Diario";
 
-    $diario.ServerSideProcessingCtrl = function(DTOptionsBuilder, DTColumnBuilder) {
-        vm.dtOptions = DTOptionsBuilder.newOptions()
-            .withOption('ajax', {
-                // Either you specify the AjaxDataProp here
-                // dataSrc: 'data',
-                url: 'lalala.json',
-                type: 'POST'
-            })
-        // or here
-        .withDataProp('data')
-            .withOption('processing', true)
-            .withOption('serverSide', true)
-            .withPaginationType('full_numbers');
-        vm.dtColumns = [
-            DTColumnBuilder.newColumn('id').withTitle('ID'),
-            DTColumnBuilder.newColumn('firstName').withTitle('First name'),
-            DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
-        ];
-    }
+    // $diario.ServerSideProcessingCtrl = function(DTOptionsBuilder, DTColumnBuilder) {
+    //     vm.dtOptions = DTOptionsBuilder.newOptions()
+    //         .withOption('ajax', {
+    //             // Either you specify the AjaxDataProp here
+    //             // dataSrc: 'data',
+    //             url: 'teste.json',
+    //             type: 'POST'
+    //         })
+    //     // or here
+    //     .withDataProp('data')
+    //         .withOption('processing', true)
+    //         .withOption('serverSide', true)
+    //         .withPaginationType('full_numbers');
+    //     vm.dtColumns = [
+    //         DTColumnBuilder.newColumn('id').withTitle('ID'),
+    //         DTColumnBuilder.newColumn('firstName').withTitle('First name'),
+    //         DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
+    //     ];
+    // }
 });
 
 
