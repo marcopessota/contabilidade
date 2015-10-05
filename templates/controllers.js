6ee9295724df3ef4edbcf8b50969091203387b26
mvmpvinicius <mvmpvinicius@gmail.com>
2015-10-05 18:53:37 -0300

    Funcionamento de Fórmula no Handsontable
    - Adicionado bibliotecas necessária para o funcionamento

--- templates/controllers.js
+++ templates/controllers.js
@@ -34,6 +34,23 @@ app.controller('diarioController', function($scope) {
     //         DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
     //     ];
     // }
+
+    var data = [
+        ["", "Ford", "Volvo", "Toyota", "Honda"],
+        ["2014", 10, 11, 12, 13],
+        ["2015", 20, 11, 14, 13],
+        ["2016", 30, 15, 12, 13]
+    ];
+
+    var container = document.getElementById('example');
+    var hot = new Handsontable(container, {
+        data: data,
+        minSpareRows: 1,
+        rowHeaders: true,
+        colHeaders: true,
+        contextMenu: true,
+        formulas: true
+    });
 });
 
 
