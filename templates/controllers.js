0d747ccb7bfe2511af27e3927712dd8fb7109cd4
mvmpvinicius <mvmpvinicius@gmail.com>
2015-10-05 17:52:14 -0300

    Inclusão do datatable(Diário)
    - Inclusão de bibliotecas
    - Inserido Diário(DataTable) na tela

--- templates/controllers.js
+++ templates/controllers.js
@@ -5,21 +5,39 @@ app.controller('appController', function($scope) {
     $app.titulo = "App";
     $app.selected_menu = "contabilidade";
 
-
-
-    $app.select_menu = function(menu){
-		$app.selected_menu = menu;
-	};
+    $app.select_menu = function(menu) {
+        $app.selected_menu = menu;
+    };
 });
 
 
 app.controller('diarioController', function($scope) {
     var $diario = this;
     $diario.titulo = "Diario";
+
+    $diario.ServerSideProcessingCtrl = function(DTOptionsBuilder, DTColumnBuilder) {
+        vm.dtOptions = DTOptionsBuilder.newOptions()
+            .withOption('ajax', {
+                // Either you specify the AjaxDataProp here
+                // dataSrc: 'data',
+                url: 'lalala.json',
+                type: 'POST'
+            })
+        // or here
+        .withDataProp('data')
+            .withOption('processing', true)
+            .withOption('serverSide', true)
+            .withPaginationType('full_numbers');
+        vm.dtColumns = [
+            DTColumnBuilder.newColumn('id').withTitle('ID'),
+            DTColumnBuilder.newColumn('firstName').withTitle('First name'),
+            DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
+        ];
+    }
 });
 
 
 app.controller('auditoriaController', function($scope) {
     var $auditoria = this;
     $auditoria.titulo = "Auditoria";
-}); 
+});
