acf897851e3da9130bf0bdeb63359f5f1c5766f0
marcopessota <marco.a.pessota@gmail.com>
2015-10-09 18:55:00 -0300

    UPDATE
    Envio de WS

--- templates/diario/diario.controller.js
+++ templates/diario/diario.controller.js
@@ -4,23 +4,60 @@ app.controller('diarioController', function($scope) {
     var $diario = this;
     $diario.titulo = "Diario";
 
-    // $diario.ServerSideProcessingCtrl = function(DTOptionsBuilder, DTColumnBuilder) {
-    //     vm.dtOptions = DTOptionsBuilder.newOptions()
-    //         .withOption('ajax', {
-    //             // Either you specify the AjaxDataProp here
-    //             // dataSrc: 'data',
-    //             url: 'teste.json',
-    //             type: 'POST'
-    //         })
-    //     // or here
-    //     .withDataProp('data')
-    //         .withOption('processing', true)
-    //         .withOption('serverSide', true)
-    //         .withPaginationType('full_numbers');
-    //     vm.dtColumns = [
-    //         DTColumnBuilder.newColumn('id').withTitle('ID'),
-    //         DTColumnBuilder.newColumn('firstName').withTitle('First name'),
-    //         DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
-    //     ];
-    // }
+    $diario.ServerSideProcessingCtrl = function($compile, $scope, $resource, DTOptionsBuilder, DTColumnBuilder) {
+        var vm = this;
+        vm.selected = {};
+        vm.selectAll = false;
+        vm.toggleAll = toggleAll;
+        vm.toggleOne = toggleOne;
+
+        var titleHtml = '<input type="checkbox" ng-model="showCase.selectAll" ng-click="showCase.toggleAll(showCase.selectAll, showCase.selected)">';
+
+
+
+        vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
+            return $resource('templates/diario/data.json').query().$promise;
+        })
+        .withOption('createdRow', function(row, data, dataIndex) {
+            // Recompiling so we can bind Angular directive to the DT
+            $compile(angular.element(row).contents())($scope);
+        })
+        .withOption('headerCallback', function(header) {
+            if (!vm.headerCompiled) {
+                // Use this headerCompiled field to only compile header once
+                vm.headerCompiled = true;
+                $compile(angular.element(header).contents())($scope);
+            }
+        })
+        .withPaginationType('full_numbers');
+
+        vm.dtColumns = [
+            DTColumnBuilder.newColumn(null).withTitle(titleHtml).notSortable()
+                .renderWith(function(data, type, full, meta) {
+                    vm.selected[full.id] = false;
+                    return '<input type="checkbox" ng-model="showCase.selected[' + data.id + ']" ng-click="showCase.toggleOne(showCase.selected)">';
+                }),
+            DTColumnBuilder.newColumn('id').withTitle('ID'),
+            DTColumnBuilder.newColumn('firstName').withTitle('First name'),
+            DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
+        ];
+         function toggleAll (selectAll, selectedItems) {
+        for (var id in selectedItems) {
+            if (selectedItems.hasOwnProperty(id)) {
+                selectedItems[id] = selectAll;
+            }
+        }
+        }
+        function toggleOne (selectedItems) {
+            for (var id in selectedItems) {
+                if (selectedItems.hasOwnProperty(id)) {
+                    if(!selectedItems[id]) {
+                        vm.selectAll = false;
+                        return;
+                    }
+                }
+            }
+            vm.selectAll = true;
+        }
+    }
 });
---
+++ ws/ajax.php
@@ -0,0 +1,9 @@
+<?php 
+
+	require_once("config.php")
+
+	$post = $_POST["_p"];
+	require_once("ajax/".$_POST["_f"].".php");
+
+
+?>
