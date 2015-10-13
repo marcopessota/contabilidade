4a50cfff9a1116777112d4f656afb45abcac32a1
mvmpvinicius <mvmpvinicius@gmail.com>
2015-10-13 16:29:16 -0300

    Teste Commit

--- templates/folha_trabalho/folha_trabalho.controller.js
+++ templates/folha_trabalho/folha_trabalho.controller.js
@@ -4,13 +4,6 @@ app.controller('folhaTrabalhoController', function($scope) {
     var $folha_trabalho = this;
     $folha_trabalho.titulo = "Folhas de Trabalho";
 
-    // var data = [
-    //     ["", "Ford", "Volvo", "Toyota", "Honda"],
-    //     ["2014", 10, 11, 12, 13],
-    //     ["2015", 20, 11, 14, 13],
-    //     ["2016", 30, 15, 12, 13]
-    // ];
-
     var container = document.getElementById('example');
     var hot = new Handsontable(container, {
         // data: data,
