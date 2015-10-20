ae39ada78e55b762c426ccb18da564a32d63b912
marcopessota <marco.a.pessota@gmail.com>
2015-10-20 14:34:52 -0200

    UPDATE
    Envio de importadores de diario
    Alterancoes no ajax principal
    Funcoes basicas implementadas no services geral

--- templates/diario/diario.controller.js
+++ templates/diario/diario.controller.js
@@ -1,63 +1,83 @@
 var app = angular.module('app');
 
-app.controller('diarioController', function($scope) {
-    var $diario = this;
-    $diario.titulo = "Diario";
-    $diario.selected = {};
-
-    $diario.ServerSideProcessingCtrl = function($compile, $scope, $resource, DTOptionsBuilder, DTColumnBuilder) {
-        var vm = this;
-        vm.selectAll = false;
-        $diario.toggleAll = toggleAll;
-        $diario.toggleOne = toggleOne;
-
-        var titleHtml = '<input type="checkbox" ng-model="diarioCtrl.selectAll" ng-click="diarioCtrl.toggleAll(diarioCtrl.selectAll, diarioCtrl.selected)">';
+app.controller('diarioController', function($scope, $http, S_vars, S_http_validate) {
+	var $diario = this;
+	$diario.titulo = "Diario";
+	$diario.action = "list";
+	$diario.selected = {};
 
 
 
-        vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-            return $resource('templates/diario/data.json').query().$promise;
-        })
-        .withOption('createdRow', function(row, data, dataIndex) {
-            // Recompiling so we can bind Angular directive to the DT
-            $compile(angular.element(row).contents())($scope);
-        })
-        .withOption('headerCallback', function(header) {
-            if (!vm.headerCompiled) {
-                // Use this headerCompiled field to only compile header once
-                vm.headerCompiled = true;
-                $compile(angular.element(header).contents())($scope);
+	$diario.importar_diario = function(){
+		console.log(S_http_validate);
+		var obj_ajax = {};
+		obj_ajax._f = "importar_diario";
+		obj_ajax._p = {"a" : 1};
+		$http.post(S_vars.url_ajax + "ajax.php", obj_ajax).success(function(data, status) {
+			var validation = S_http_validate.validate_success(data.error, status);
+			if(validation == true){
+				$diario.preview_import_items = data.value;
+				$diario.action = "import";
+            }else{
+            	alert(validation);
             }
-        })
-        .withPaginationType('full_numbers');
+        });
+	}
 
-        vm.dtColumns = [
-            DTColumnBuilder.newColumn(null).withTitle(titleHtml).notSortable()
-                .renderWith(function(data, type, full, meta) {
-                    $diario.selected[full.id] = false;
-                    return '<input type="checkbox" ng-model="diarioCtrl.selected[' + data.id + ']" ng-click="diarioCtrl.toggleOne(diarioCtrl.selected)">';
-                }),
-            DTColumnBuilder.newColumn('id').withTitle('ID'),
-            DTColumnBuilder.newColumn('firstName').withTitle('First name'),
-            DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
-        ];
-        function toggleAll (selectAll, selectedItems) {
-        for (var id in selectedItems) {
-            if (selectedItems.hasOwnProperty(id)) {
-                selectedItems[id] = selectAll;
-            }
-        }
-        }
-        function toggleOne (selectedItems) {
-            for (var id in selectedItems) {
-                if (selectedItems.hasOwnProperty(id)) {
-                    if(!selectedItems[id]) {
-                        vm.selectAll = false;
-                        return;
-                    }
-                }
-            }
-            vm.selectAll = true;
-        }
-    }
+
+	$diario.ServerSideProcessingCtrl = function($compile, $scope, $resource, DTOptionsBuilder, DTColumnBuilder) {
+		var vm = this;
+		vm.selectAll = false;
+		$diario.toggleAll = toggleAll;
+		$diario.toggleOne = toggleOne;
+
+		var titleHtml = '<input type="checkbox" ng-model="diarioCtrl.selectAll" ng-click="diarioCtrl.toggleAll(diarioCtrl.selectAll, diarioCtrl.selected)">';
+
+
+
+		vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
+			return $resource('templates/diario/data.json').query().$promise;
+		})
+		.withOption('createdRow', function(row, data, dataIndex) {
+			// Recompiling so we can bind Angular directive to the DT
+			$compile(angular.element(row).contents())($scope);
+		})
+		.withOption('headerCallback', function(header) {
+			if (!vm.headerCompiled) {
+				// Use this headerCompiled field to only compile header once
+				vm.headerCompiled = true;
+				$compile(angular.element(header).contents())($scope);
+			}
+		})
+		.withPaginationType('full_numbers');
+
+		vm.dtColumns = [
+			DTColumnBuilder.newColumn(null).withTitle(titleHtml).notSortable()
+				.renderWith(function(data, type, full, meta) {
+					$diario.selected[full.id] = false;
+					return '<input type="checkbox" ng-model="diarioCtrl.selected[' + data.id + ']" ng-click="diarioCtrl.toggleOne(diarioCtrl.selected)">';
+				}),
+			DTColumnBuilder.newColumn('id').withTitle('ID'),
+			DTColumnBuilder.newColumn('firstName').withTitle('First name'),
+			DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
+		];
+		function toggleAll (selectAll, selectedItems) {
+		for (var id in selectedItems) {
+			if (selectedItems.hasOwnProperty(id)) {
+				selectedItems[id] = selectAll;
+			}
+		}
+		}
+		function toggleOne (selectedItems) {
+			for (var id in selectedItems) {
+				if (selectedItems.hasOwnProperty(id)) {
+					if(!selectedItems[id]) {
+						vm.selectAll = false;
+						return;
+					}
+				}
+			}
+			vm.selectAll = true;
+		}
+	}
 });
--- templates/services.js
+++ templates/services.js
@@ -1,1 +1,44 @@
 var app = angular.module('app');
+
+app.factory('S_http_validate', ['S_errors_message', function(S_errors_message) {
+	$http_validate = this;
+
+	$http_validate.validate_success = function(data, status){
+		if(status == 200){
+			if(data == "no_error"){
+				return true;
+			}else{
+				return S_errors_message.error(data);
+			}
+		}else{
+			return S_errors_message.error(status);
+		}
+	};
+
+	return $http_validate;
+}]);
+
+app.factory('S_errors_message', [function() {
+	$S_errors_message = this;
+
+	$S_errors_message.e = {};
+	$S_errors_message.e["500"] = "Script error (Internal Server Error)";
+	$S_errors_message.e["404"] = "File not found";
+	$S_errors_message.e["erro"] = "Script error";
+	$S_errors_message.e["required_field"] = "Required Field";
+
+	$S_errors_message.error = function(e){
+		return $S_errors_message.e[e];
+	}
+	return $S_errors_message;
+}]);
+
+
+
+app.factory('S_vars', [function() {
+	$S_vars = this;
+
+	$S_vars.url_ajax = "ws/";
+
+	return $S_vars;
+}]);
