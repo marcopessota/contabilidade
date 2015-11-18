6fa9dec3bd06f150ceb756c7dcbb15edaadc7382
mvmpvinicius <mvmpvinicius@gmail.com>
2015-11-18 11:47:28 -0200

    Envio de dados
    - Envio de dados da tela de balancete para folha de trabalho

--- templates/balancete/balancete.controller.js
+++ templates/balancete/balancete.controller.js
@@ -14,5 +14,23 @@ app.controller('balanceteController', function($scope, $http, S_vars, $sce) {
         });
     }
 
+    $balancete.sendData = function() {
+        var nome_folha_trabalho = prompt('Insira o nome da Folha de Trabalho');
+
+        if (nome_folha_trabalho == '') return false;
+
+        var rows = getSelRows();
+
+        var obj_ajax = {};
+        obj_ajax._f = 'envia_folha_trabalho';
+        obj_ajax._p = {
+            rows: rows,
+            nome_folha_trabalho: nome_folha_trabalho
+        };
+        $http.post(S_vars.url_ajax + 'ajax.php', obj_ajax).success(function(data, status) {
+            // console.log(data);
+        });
+    }
+
     $balancete.iniciar_master_detail();
 });
