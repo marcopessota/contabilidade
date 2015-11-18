d6f3d21b6c6817fc85c6a3768597cff9b2175108
marcopessota <marco.a.pessota@gmail.com>
2015-11-18 16:54:31 -0200

    UPDATE
    Processo de gerar balancete

--- ws/ajax/inicia_balancete.php
+++ ws/ajax/inicia_balancete.php
@@ -1,17 +1,15 @@
 <?php
     require_once("phpGrid/conf.php"); // relative path to conf.php without leading slash
 
-    $dg = new C_DataGrid("SELECT * FROM diario", "id", "diario");
-    // $sdg = new C_DataGrid("SELECT * FROM teste_details", "id", "teste_details");
-    // $sdgg = new C_DataGrid("SELECT * FROM teste_teste_details", "id", "teste_teste_details");
+    $dg = new C_DataGrid("SELECT account, debt, credit, outstanding_balance, id, id_diario FROM trial_balance", "id", "Balancete");
 
-    // enable edit
-    $dg->enable_edit("INLINE", "CRUD");
     $dg->set_multiselect(true);
-    // $sdg->enable_edit("INLINE", "CRUD");
-    // $sdgg->enable_edit("INLINE", "CRUD");
-    
-    // $dg->set_masterdetail($sdg, "testeId");
-    // $sdg->set_masterdetail($sdgg, "id");
+    $dg->set_col_hidden('id');
+    $dg->set_col_hidden('id_diario');
+    $dg->set_col_title("account", "Conta");
+    $dg->set_col_title("outstanding_balance", "Saldo");
+    $dg->set_col_title("debt", "Débito");
+    $dg->set_col_title("credit", "Crédito");
+
     $dg->display();
 ?>
