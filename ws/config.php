9d56c20a5218980157d14bca9014befa847f65e6
mvmpvinicius <mvmpvinicius@gmail.com>
2015-12-07 15:23:01 -0200

    Teste

--- ws/config.php
+++ ws/config.php
@@ -3,19 +3,23 @@
 error_reporting(0);
 
 function __autoload($class_name) {
-    require_once 'lib/classes/' . strtolower($class_name) . '.php';
+    // require_once 'lib/classes/' . strtolower($class_name) . '.php';
+    $filename = 'lib/classes/' . strtolower($class_name) . '.php';
+	if(file_exists($filename)){
+    	require_once $filename;
+    }
 }
 
 
 // CONSTANTES
 
-// define("CONNECTOR_DB", "MYSQL");
-define("CONNECTOR_DB", "MONGODB");
+define("CONNECTOR_DB", "MYSQL");
+// define("CONNECTOR_DB", "MONGODB");
 
 define("SERVER_MYSQL", "localhost");
 define("USER_MYSQL", "root");
 define("PASSWORD_MYSQL", "");
-define("DATABASE_MYSQL", "test");
+define("DATABASE_MYSQL", "contabilidade");
 
 // define("SERVER_MONGODB", "127.0.0.1");
 // define("USER_MONGODB", "");
